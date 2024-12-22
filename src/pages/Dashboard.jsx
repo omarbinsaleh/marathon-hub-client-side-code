804e49af19bd098a623144798ee53d0b2fafcd1d
omarbinsaleh <omarbinsaleh44@gmail.com>
2024-12-23 00:53:10 +0400

    modified the dashboard's menu section width

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div className='min-h-screen w-full flex'>
-      <div className='hidden md:block'>
+      <div className='hidden md:block w-[130px]'>
          dashboard menu
       </div>
       <div className='flex-1 border border-red-500'>
